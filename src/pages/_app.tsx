5a835c8ed36c2baff1e2204220ec51f44202027d
Matheus Henrique <mattsilvahenrique102@gmail.com>
2022-06-20 13:25:20 -0300

    autenticação do github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import { Header } from '../components/Header';
 import '../styles/global.scss';
+import { SessionProvider as NextAuthProvider } from 'next-auth/react';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   );
 }
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { GetStaticProps } from 'next';
+
 import Head from 'next/head';
 import Image from 'next/image';
 
@@ -5,8 +7,16 @@ import styles from './home.module.scss';
 import Avatar from '../../public/images/avatar.svg';
 
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceID: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -21,9 +31,9 @@ export default function Home() {
           </h1>
           <p>
             Get the access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceID={product.priceID} />
         </section>
 
         <Image src={Avatar} alt="Girl coding" />
@@ -31,3 +41,22 @@ export default function Home() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1LCk62KkiQHprDDVm96nQbWz');
+
+  const product = {
+    priceID: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 Hours
+  };
+};
