abaf80abc8b98b0dd00ec79eabca7a5483753bfc
Matheus Henrique <mattsilvahenrique102@gmail.com>
2022-10-20 15:58:57 -0300

    feat: initial project setup

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    colors: {
+      blue: {
+        500: '#61DCFB',
+      },
+
+      green: {
+        500: '#04D361',
+      },
+
+      yellow: {
+        500: '#EBA417',
+      },
+
+      gray: {
+        100: '#E1E1E6',
+        300: '#A8A8B3',
+        700: '#1F2729',
+        900: '#121414',
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
